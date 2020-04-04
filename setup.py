a1ee4e2eb79cfd4fb4500b9f16512a19efcfad3c
Lawrence He <33579950+Lawhy@users.noreply.github.com>
2020-04-04 04:56:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="multi-task-nmt-lawhy", # Replace with your own username
+    name="multi-task-nmt-lawhy",
     version="0.0.5",
     author="Yuan (Lawrence) He",
     author_email="lawhy0729@gmail.com",
-    description="Implementation of the Multi-task Neural Machine Transliteration System",
+    description="Pytorch Implementation of the Multi-task Neural Machine Transliteration System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Lawhy/Multi-task-NMT",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
