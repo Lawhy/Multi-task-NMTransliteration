c9636ce0611dfb0b29167ba179975a5224e2a0ce
lawhy <lawhy0729@gmail.com>
2020-04-08 06:24:20 +0100

    add multi_task_seq2seq.py

--- mnmt/model/__init__.py
+++ mnmt/model/__init__.py
@@ -1,1 +1,3 @@
 from mnmt.model.basic_seq2seq import Seq2Seq
+from mnmt.model.multi_task_seq2seq import Seq2MultiSeq
+
---
+++ mnmt/model/multi_task_seq2seq.py
@@ -0,0 +1,25 @@
+from mnmt.inputter import ArgsFeeder
+from mnmt.encoder import BasicEncoder
+from mnmt.decoder import BasicDecoder
+from mnmt.trainer.utils import create_mask
+import torch.nn as nn
+from typing import List
+
+
+class Seq2MultiSeq(nn.Module):
+
+    def __init__(self, args_feeder: ArgsFeeder, encoder: BasicEncoder,
+                 decoder_list: List[BasicDecoder], teacher_forcing_ratio):
+        super().__init__()
+        self.args_feeder = args_feeder
+        self.encoder = encoder
+        self.decoder_list = decoder_list
+        self.teacher_forcing_ratio = teacher_forcing_ratio
+        self.device = self.decoder.device
+
+    def forward(self, src, src_lens, *trg):
+        encoder_outputs, encoder_final_state = self.encoder(src, src_lens)
+        mask = create_mask(src, self.args_feeder.src_pad_idx)
+        predictions = [decoder(trg, encoder_outputs, encoder_final_state, mask,
+                               self.teacher_forcing_ratio) for decoder in self.decoder_list]
+        return tuple(predictions)
