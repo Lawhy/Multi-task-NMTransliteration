dfd488c0e311ddadb48388c647a3df39b786306b
lawhy <lawhy0729@gmail.com>
2020-04-10 07:56:00 +0100

    Add BeamSearch Decoding.

--- mnmt/decoder/__init__.py
+++ mnmt/decoder/__init__.py
@@ -2,4 +2,4 @@ from mnmt.decoder.bridge import BridgeLayer
 from mnmt.decoder.basic_feed_forward_decoder import BasicFeedForwardDecoder
 from mnmt.decoder.basic_decoder import BasicDecoder
 from mnmt.decoder.greedy_decoder import GreedyDecoder
-
+from mnmt.decoder.beam_decoder import BeamDecoder
