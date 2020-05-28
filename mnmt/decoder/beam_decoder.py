8ea2d9e64d1f63c5dbdadd748c34039843e1846d
lawhy <lawhy0729@gmail.com>
2020-05-29 00:51:05 +0100

    beam

--- mnmt/decoder/beam_decoder.py
+++ mnmt/decoder/beam_decoder.py
@@ -173,11 +173,11 @@ class BeamDecoder(BasicDecoder):
             # Re-order the variables with the back pointer
             print(t)
             current_output = nw_output[t].index_select(0, t_predecessors)
-            print(nw_hidden[t][0].shape, t_predecessors.shape)
             if lstm:
                 current_hidden = tuple([h.index_select(0, t_predecessors) for h in nw_hidden[t]])
             else:
                 current_hidden = nw_hidden[t].index_select(0, t_predecessors)
+            print(symbols[t].shape)
             current_symbol = symbols[t].index_select(0, t_predecessors)
             # Re-order the back pointer of the previous step with the back pointer of
             # the current step
