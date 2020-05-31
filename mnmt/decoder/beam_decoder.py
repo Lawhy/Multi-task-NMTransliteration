0d82e2b23a241da3d4cf648e7e8e651ca4220558
lawhy <lawhy0729@gmail.com>
2020-05-31 12:13:32 +0100

    beam-search-implemented

--- mnmt/decoder/beam_decoder.py
+++ mnmt/decoder/beam_decoder.py
@@ -199,7 +199,7 @@ class BeamDecoder(BasicDecoder):
             y_hat[:, i, :] = end_node.y_hat_path.squeeze(1)
             print("Maximum index is {}".format(max_ind))
 
-            if t == 10:
+            if i == 10:
                 break
 
         assert True == False
