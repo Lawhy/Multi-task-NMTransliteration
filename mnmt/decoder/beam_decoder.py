0f376def8d3b59bc160ba87b0355886ef65df399
lawhy <lawhy0729@gmail.com>
2020-05-31 03:05:50 +0100

    beam-search-implemented

--- mnmt/decoder/beam_decoder.py
+++ mnmt/decoder/beam_decoder.py
@@ -132,9 +132,9 @@ class BeamDecoder(BasicDecoder):
 
                 scores_t = scores_topk.t().expand(-1, self.trg_vocab_size) \
                     .reshape(1, self.beam_size * self.trg_vocab_size)  # scores from previous time-step
-                scores_t += y_hat_i_t_full.clone()
+                # scores_t += y_hat_i_t_full.clone()
 
-                scores_topk, indices = torch.topk(scores_t,
+                scores_topk, indices = torch.topk(scores_t + y_hat_i_t_full,
                                                   dim=1, k=self.beam_size)  # [1, beam_size]
                 prev_node_inds = [ind // self.trg_vocab_size for ind in indices[0]]  # know which node belongs to
                 new_batch_nodes = []
