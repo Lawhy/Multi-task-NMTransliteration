ea1b697a13cadc66186b9ecdf0abdeb45ea166a9
lawhy <lawhy0729@gmail.com>
2020-06-01 11:04:13 +0100

    beam-search-implemented

--- mnmt/model/multi_task_seq2seq.py
+++ mnmt/model/multi_task_seq2seq.py
@@ -18,7 +18,10 @@ class Seq2MultiSeq(nn.Module):
     def forward(self, src, src_lens, *trg):
         encoder_outputs, encoder_final_state = self.encoder(src, src_lens)
         mask = create_mask(src, self.args_feeder.src_pad_idx)
-        predictions = [self.decoder_list[i](trg[i], encoder_outputs, encoder_final_state, mask,
-                                            self.teacher_forcing_ratio) for i in range(len(self.decoder_list))]
+        output_pred = []
+        for i in range(len(self.decoder_list)):
+            output, pred = self.decoder_list[i](trg[i], encoder_outputs, encoder_final_state, mask,
+                                                self.teacher_forcing_ratio)
+            output_pred += [output, pred]
         # output1, pred1, output2, pred2 ...
-        return tuple(predictions)
+        return tuple(output_pred)
